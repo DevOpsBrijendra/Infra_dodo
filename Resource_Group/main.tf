be9a63db194228b02969cc6979d5b4a456567c85
unknown <brijendrayadav014@gmail.com>
2025-07-11 20:29:02 +0530

    rg-dodo

---
+++ Resource_Group/main.tf
@@ -0,0 +1,21 @@
+terraform {
+    required_providers {
+        azurerm ={
+            source = "hashicorp/azurerm"
+            version = "4.33.0"
+    }
+    }
+}
+provider "azurerm" {
+    features {}
+    subscription_id = "180d2ab6-0ded-4071-b4c9-3ebefd722b58"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg-dodo"
+  location = "West Europe"
+
+  tags = {
+    environment = "Terraform"
+  }
+}
